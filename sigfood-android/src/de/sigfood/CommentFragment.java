3237a4d15645bc13cb02233f488a10c226c222ec
Chris Sp <chrspangler@gmx.de>
2013-11-26 11:17:14 +0100

    Removed unneeded imports in CommentFragment

--- sigfood-android/src/de/sigfood/CommentFragment.java
+++ sigfood-android/src/de/sigfood/CommentFragment.java
@@ -6,20 +6,6 @@ package de.sigfood;
 // Handles commenting
 // ----------------------------------------
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
